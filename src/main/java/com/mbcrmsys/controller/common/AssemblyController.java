46c06f3629997a3a37b5d2c8e902ca513e28698f
TACHAI <1206966083@qq.com>
2018-03-13 22:11:00 +0800

    修改了一些bug

--- src/main/java/com/mbcrmsys/controller/common/AssemblyController.java
+++ src/main/java/com/mbcrmsys/controller/common/AssemblyController.java
@@ -13,8 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * 组件
@@ -35,26 +38,38 @@ public class AssemblyController {
     @RequestMapping("getcus.do")
     public ServerResponse<List<OpptionsVo>> getCus(HttpSession session){
         List<Customer> list=customerService.selectCus();
-        List<OpptionsVo> opptionsVoList=list.stream()
-                .map(e->e.getCusCkId())
-                .collect(Collectors.toList());
-        return null;
+        List<OpptionsVo> opptionsVoList = new ArrayList<>();
+        list.forEach(e->{
+            OpptionsVo opptionsVo=new OpptionsVo();
+            opptionsVo.setValue(e.getCusId());
+            opptionsVo.setText(e.getCusName());
+            opptionsVoList.add(opptionsVo);
+        });
+        return ServerResponse.createBySuccess(opptionsVoList,"获得客户列表");
     }
     @RequestMapping("getrank.do")
     public ServerResponse<List<OpptionsVo>> getRank(HttpSession session){
         List<CustomerRank> list=customerRankSerivce.selectRank();
         List<OpptionsVo> opptionsVoList=list.stream()
-                .map(e->e.getCkId())
-                .map(e->e.ge)
-                .collect(Collectors.toList());
-        return null;
+                .map(e->{
+                    OpptionsVo opptionsVo=new OpptionsVo();
+                    opptionsVo.setValue(e.getCkId());
+                    opptionsVo.setText(e.getCkName());
+                    return opptionsVo;
+                }).collect(Collectors.toList());
+        return ServerResponse.createBySuccess(opptionsVoList,"获得客户等级列表");
     }
     @RequestMapping("getarea")
     public ServerResponse<List<OpptionsVo>> getArea(HttpSession session){
         List<Area> list=areaService.selectArea();
         List<OpptionsVo> opptionsVoList=list.stream()
-                .map(e->e.getAreId())
-                .collect(Collectors.toList());
-        return null;
+                .map(e->{
+                    OpptionsVo opptionsVo=new OpptionsVo();
+                    opptionsVo.setValue(e.getAreId());
+                    opptionsVo.setText(e.getAreName());
+                    return opptionsVo;
+                })
+                .collect(toList());
+        return ServerResponse.createBySuccess(opptionsVoList,"获得地址列表");
     }
 }
